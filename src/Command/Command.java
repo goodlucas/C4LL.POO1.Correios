84870c3c80e366a89edcae2e1c502a16d915483c
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-05-22 23:07:04 -0400

    OptionParser parseString changes
    Change-Id: Iae5bac57a5364a7bc0d4eb2f0f13c62151d47df0

--- src/Command/Command.java
+++ src/Command/Command.java
@@ -11,16 +11,22 @@ public class Command {
 	private String				name;
 	private ArrayList<Option>	options;
 	
-	Command(String name) {
-		this.name = name;
+	public Command(String commandLine) {
 		options = new ArrayList<Option>();
+		name = new String(commandLine).split(" ")[0];
+		if (name.equals("inbox")) {
+			new OptionParser().addOption("r", "read", false, "List read messages")
+							  .addOption("u", "unread", false, "List unread messages")
+							  .addOption("a", "all", false, "List all messages")
+							  .parseString(commandLine, options);
+		}
 	}
 	
-	public Command addOption(Option o) {
-		if (o != null)
-			options.add(o);
-		return this;
-	}
+//	public Command addOption(Option o) {
+//		if (o != null)
+//			options.add(o);
+//		return this;
+//	}
 	
 	public Option getOption(int index) {
 		return options.get(index);
