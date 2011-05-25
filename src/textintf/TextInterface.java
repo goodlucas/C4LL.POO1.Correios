5870af4378e7b7d2f50fa3e82602386bc3acfa5c
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-05-25 10:30:17 -0400

    TextInterface documentation added
    Change-Id: I0d49b56c93a0d98e034e180feb4e05cf5ded73bc

--- src/textintf/TextInterface.java
+++ src/textintf/TextInterface.java
@@ -1,5 +1,9 @@
 package textintf;
 
+/**
+ * End-user text interface. This class translate a command line into an
+ * action in the post office program.
+ */
 public final class TextInterface {
 
 }
