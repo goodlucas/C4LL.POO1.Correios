f8d40268441d08b6a2958c33399571bec32f7a7a
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-05-28 00:03:25 -0400

    Added sequence UML (Activity Diagram)
    Change-Id: I5355f3b846efa280f288c913f087fbb76192b5e2

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,23 +6,9 @@ import textintf.TextInterface;
 public final class Main {
 
 	/**
-	 * @param args
+	 * Main executation.
 	 */
 	public static void main(String[] args) {
-//		Reader rd = new Reader();
-//		
-//		TerminalCommand recv = rd.readCommand();
-//		
-//		if (recv instanceof InboxCommand) {
-//			InboxCommand	ic = (InboxCommand) recv;
-//			
-//			System.out.println("--all = " + ic.all);
-//			System.out.println("--read = " + ic.read);
-//			System.out.println("--unread = " + ic.unread);
-//			System.out.println("Filtros: " + ic.filtros.toString());
-//		} else {
-//			System.out.println("Algo deu errado!");
-//		}
 		Server			server = new Server("unicentro.br");		
 		TextInterface	intf = new TextInterface(server);
 		
