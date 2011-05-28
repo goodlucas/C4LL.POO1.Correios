c79322e1dbc1bcdfb161c30c90bb1b537b7099db
Paulo Roberto Urio <jimmyskull@gmail.com>
2011-05-27 23:44:49 -0400

    Add documentation for ShowCOmmandHelp
    Change-Id: I9be6b4d67d8e8f9abf21f5c9c97b581d92896943

--- src/textintf/command/ShowCommandHelper.java
+++ src/textintf/command/ShowCommandHelper.java
@@ -5,7 +5,15 @@ import textintf.Core;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 
+/**
+ * Class for help-relationed information. This class have just static methods,
+ * tough. 
+ */
 public final class ShowCommandHelper {
+	/**
+	 * Show help command for a given command.
+	 * @param cmd	What command should get help.
+	 */
 	public static void showHelp(String cmd) {
 		for (TerminalCommand tc: CommandCollection.COMMANDS) {
 			if (tc.getCommand().getName().equals(cmd)) {
@@ -24,12 +32,20 @@ public final class ShowCommandHelper {
 		System.out.println("Não há ajuda para este comando.");
 	}
 	
+	/**
+	 * @return	String "command --help"
+	 */
 	private static String getExample() {
 		return new InboxCommand().getName() + " --help";
 	}
 	
+	/**
+	 * Show avaiable commands. If the user is currently logged, all commands
+	 * will be showed. Otherwise, only unlogged commands.
+	 * @param core	Core instance.
+	 */
 	public static void showCommands(Core core) {
-		System.out.println("Ajuda do programa PostOffice");
+		System.out.println("Ajuda do programa " + core.getProgramName());
 		System.out.println("Para ver a ajuda de um comando, use a opção --help");
 		System.out.println("Exemplo: " + getExample());
 		for (TerminalCommand tc: CommandCollection.COMMANDS) {
